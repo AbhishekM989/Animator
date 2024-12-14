97eebef4422eb6ac5b0ec2fdf6fd2220b88e2fa2
AbhishekM989 <abhishekmaurya2510@gmail.com>
2024-12-14 23:16:25 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("alertbutton");
+
+button.addEventListener('click', function() {
+    alert("ALERTTT !!!!!. You are not supposed to click that button. You have to love Mr.Abhishek Maurya and that's your right.!!! 😤")
+})
