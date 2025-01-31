dd0ffb37cc5874cb38a91fbbc90e3d3165154dd5
AbhishekM989 <abhishekmaurya2510@gmail.com>
2025-02-01 00:10:07 +0530

    Patch Update

--- script.js
+++
@@ -1,5 +0,0 @@
-const button = document.getElementById("alertbutton");
-
-button.addEventListener('click', function() {
-    alert("ALERTTT !!!!!. You are not supposed to click that button. You have to love Mr.Abhishek Maurya and that's your right.!!! 😤")
-})
